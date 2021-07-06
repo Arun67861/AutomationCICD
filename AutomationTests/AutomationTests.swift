d31a21cd42af797ec69fdfabe5953da44fcba976
Arun <arun.kumar4@publicissapient.com>
2021-07-06 20:16:45 +0530

    Added comments

--- AutomationTests/AutomationTests.swift
+++ AutomationTests/AutomationTests.swift
@@ -27,6 +27,7 @@ class AutomationTests: XCTestCase {
     }
     
     func testAutomation1() {
+        //Update comments
         let text = viewModel.automationText
       XCTAssertEqual(text, "AutomationText")
     }
